d64dba3a29d916a9996598fce88229d6a9bfe45a
angelinacepurnaja <angelina.cepurnaja@email.cz>
2026-05-19 21:39:46 +0200

    Založení třídy Had a tvoření Hada

---
+++ Snake/Had.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Snake
+{
+    internal class Had
+    {
+        public List<Point> Telo { get; set; }
+
+        
+        public Had()
+        {
+            Telo = new List<Point>();
+
+           
+            Telo.Add(new Point(10, 10)); 
+            Telo.Add(new Point(9, 10));  
+            Telo.Add(new Point(8, 10)); 
+        }
+    }
+}
