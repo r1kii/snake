b8a2fe9b800f76105f652d2fc8d4866d5bc1e8aa
angelinacepurnaja <angelina.cepurnaja@email.cz>
2026-06-04 21:48:31 +0200

    Přidání ovládání v Program.cs

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Threading; 
+
 namespace Snake
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-        }
+          
+            Had had = new Had();
 
+            Console.CursorVisible = false; 
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo klavesa = Console.ReadKey(true);
+
+                    if (klavesa.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+
+                    switch (klavesa.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            had.AktualniSmer = Smer.Up;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            had.AktualniSmer = Smer.Down;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            had.AktualniSmer = Smer.Left;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            had.AktualniSmer = Smer.Right;
+                            break;
+                    }
+                }
+
+                had.Pohyb();
+                
+                Thread.Sleep(100);
+            }
+        }
     }
 }
